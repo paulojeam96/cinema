2ad90a912c7ac366c9fb6ab856b4704dad6c4935
Paulo <Paulo@PauloJeam.home>
2015-03-20 21:24:57 -0300

    dao's

---
+++ projetoCinema/src/java/com/br/lp2/cinema/model/DAO/ListaIngressoDAO.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.lp2.cinema.model.DAO;
+
+import com.br.lp2.cinema.model.javabeans.ListaIngresso;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Paulo
+ */
+public interface ListaIngressoDAO {
+    //CRUD
+    //C - CREATE
+    public boolean insertListaIngresso(ListaIngresso listaIngresso);
+    
+    //R - READ
+    public ArrayList<ListaIngresso> readListaIngresso();
+    public ListaIngresso readAtorById(int id);
+    
+    
+    //U - UPDATE
+    public boolean updateListaIngresso(int id, ListaIngresso listaIngresso);
+    
+    //D - DELETE
+    public boolean deleteListaIngresso(ListaIngresso listaIngresso);
+    public boolean deleteListaIngresso(int id);
+}
---
+++ projetoCinema/src/java/com/br/lp2/cinema/model/DAO/ListaIngressoDAOConcreto.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.lp2.cinema.model.DAO;
+
+import com.br.lp2.cinema.model.javabeans.ListaIngresso;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Paulo
+ */
+public class ListaIngressoDAOConcreto implements ListaIngressoDAO{
+
+    @Override
+    public boolean insertListaIngresso(ListaIngresso listaIngresso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<ListaIngresso> readListaIngresso() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ListaIngresso readAtorById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateListaIngresso(int id, ListaIngresso listaIngresso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteListaIngresso(ListaIngresso listaIngresso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteListaIngresso(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
