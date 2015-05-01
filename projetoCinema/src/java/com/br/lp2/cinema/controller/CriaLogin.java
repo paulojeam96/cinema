6295a90c7d3efb3351bd9b7407cda6dd0d415347
Paulo <Paulo@PauloJeam.home>
2015-05-01 17:44:14 -0300

    1ªParte de login

---
+++ projetoCinema/src/java/com/br/lp2/cinema/controller/CriaLogin.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.lp2.cinema.controller;
+
+import com.br.lp2.cinema.model.DAO.AtendenteDAO;
+import com.br.lp2.cinema.model.DAO.AtendenteDAOConcreto;
+import com.br.lp2.cinema.model.DAO.GerenteDAO;
+import com.br.lp2.cinema.model.DAO.GerenteDAOConcreto;
+import com.br.lp2.cinema.model.javabeans.Atendente;
+import com.br.lp2.cinema.model.javabeans.Gerente;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Paulo
+ */
+public class CriaLogin {
+    private String nome;
+    private String senha;
+
+    public CriaLogin(String nome, String senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public boolean isGerente(){
+        GerenteDAO g = new GerenteDAOConcreto();
+        ArrayList<Gerente> lista = g.readGerente();
+        boolean v = false;
+        for (Gerente gr : lista) {
+            if(gr.getNome().equals(nome) && gr.getSenha().equals(senha)){    
+                v = true;
+                break;
+            } 
+        }
+        return v;
+    }
+    
+    public boolean isAtendente(){
+        AtendenteDAO a = new AtendenteDAOConcreto();
+        ArrayList<Atendente> lista = a.readAtendente();
+        
+        boolean v = false;
+        if(!v){
+            for (Atendente at : lista) {
+                if(at.getNome().equals(nome) && at.getSenha().equals(senha)){    
+                    v = true;
+                    break;
+                } 
+            }
+        }
+        return v;
+    }
+    
+    
+}
